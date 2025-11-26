22d1f5d3eaae03b4a6c016a69cce7ae808a88d1a
Tankala Sanjana <tankalasanjana@gmail.com>
2025-11-25 22:31:43 -0600

    api_hardcode

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 // Backend URL from .env
-const API_BASE = "https://campus-resource-app.onrender.com";
-
+// const API_BASE = "https://campus-resource-app.onrender.com";
+const API_BASE = "http://localhost:5000";
 const api = axios.create({
   baseURL: `${API_BASE}/api`,
   withCredentials: true,
